6ce44c645613d988a5312b510f3c612060dcf0e2
SerKin0 <sergey.skor007@gmail.com>
2025-11-27 18:25:05 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var flag=false;
+
+function playAudio(url) {
+    if (!flag) {
+        var audio = new Audio(url);
+        audio.loop = true;
+        audio.play();
+        flag=true;
+    }
+}
+
